4daf16e053cae2d8e9f87f104820961a1e645f29
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-16 22:20:05 +0530

    uncontroled component: form submission

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,18 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 const Login = () => {
+  const [state, setstate] = useState({});
+  //using Ref in the class based component to make a uncontroled component- a component whose state is not directly controlled by React.
+  // constructor(props) {
+  //      super(props);
+  //     this.emailInputRef = React.createRef();
+  //     this.passwordInputRef = React.createRef();
+  // }
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Called Submit in login', event);
+  };
   return (
-    <form className="login-form">
+    <form className="login-form" onSubmit={handleFormSubmit}>
       <span className="login-signup-header">Log In</span>
       <div className="field">
         <input type="email" name="email" placeholder="Email" required />
