2dded63615293fc92389b12f16c077d0fbf3decc
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-16 22:45:44 +0530

    controlled component: form submnission

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 const Login = () => {
-  const [state, setstate] = useState({});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   //using Ref in the class based component to make a uncontroled component- a component whose state is not directly controlled by React.
   // constructor(props) {
   //      super(props);
@@ -9,19 +10,27 @@ const Login = () => {
   // }
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    console.log('email', email, 'password', password);
     console.log('Called Submit in login', event);
   };
   return (
     <form className="login-form" onSubmit={handleFormSubmit}>
       <span className="login-signup-header">Log In</span>
       <div className="field">
-        <input type="email" name="email" placeholder="Email" required />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
       </div>
       <div className="field">
         <input
           type="password"
           name="password"
           placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
           required
         />
       </div>
