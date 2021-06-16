82fc8c951db59de45caa5406e11c0895f7f87dc4
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-16 18:53:03 +0530

    add the react-router-dom, add first route

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
-import { PostsList } from './';
+import { PostsList, Navbar } from './';
 function App(props) {
   const { posts } = props;
   useEffect(() => {
@@ -11,60 +12,16 @@ function App(props) {
   console.log('state in app.js', props);
 
   return (
-    <div className="App">
-      <nav className="nav">
-        <div className="left-nav">
-          <img
-            src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
-            alt="logo"
-          />
-        </div>
-        <div className="search-container">
-          <img
-            className="search-icon"
-            src="https://image.flaticon.com/icons/svg/483/483356.svg"
-            alt="search-icon"
-          />
-          <input type="text" placeholder="Search anything here" />
-          <div className="search-results">
-            <ul>
-              <li className="search-results-row">
-                <img
-                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                  alt="user-dp"
-                />
-                <span>John Doe</span>
-              </li>
-              <li className="search-results-row">
-                <img
-                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-                  alt="user-dp"
-                />
-                <span>John Doe</span>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="right-nav">
-          <div className="user">
-            <img
-              src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
-              alt="user-dp"
-              id="user-dp"
-            />
-            <span>John Doe</span>
-          </div>
-          <div className="nav-links">
-            <ul>
-              <li>Login</li>
-              <li>Log Out</li>
-              <li>Register</li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <PostsList posts={posts} />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <PostsList posts={posts} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 let mapStateToProps = (state) => {
