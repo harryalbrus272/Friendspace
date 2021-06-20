e164841fe22a050845f5a9095aab840e1bbd998d
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-06-21 01:01:55 +0530

    websocket demo loaded with error

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import { useState } from 'react';
 import '../chat.css';
+import io from 'socket.io-client';
+import { connect } from 'react-redux';
 
-const Chat = () => {
+const Chat = (props) => {
+  const { user, dispatch } = props;
   const [message, setMessage] = useState([]); //content: 'some messages', self: true
   const [typedMessage, setTypedMessage] = useState('');
+  const socket = io.connect('ws://54.237.158.65:5000');
+  const userEmail = user.email;
   const handleSubmit = () => {
-
+    if (typedMessage && userEmail) {
+      socket.emit('send_message', {
+        message: typedMessage,
+        user_email: userEmail,
+        chatroom: 'friendspace',
+      });
+    }
   };
+  const setupConnections = () => {
+    socket.on('connect', () => {
+      console.log('connection established');
+      socket.emit('join_room', {
+        user_email: userEmail,
+        chatroom: 'friendspace',
+      });
+
+      socket.on('user_joined', (data) => {
+        console.log('new user joined');
+      });
+      socket.on('receive_message', (data) => {
+        console.log('new user joined');
+        //add message to state
+        const messageObj = {};
+        messageObj.content = data.message;
+        if (data.user_email === userEmail) {
+          messageObj.self = true;
+        }
+        setMessage({
+          messages: [...message, messageObj],
+          typeMessage: '',
+        });
+      });
+    });
+  };
+  if (userEmail) {
+    setupConnections();
+  }
+
   return (
     <div className="chat-container">
       <div className="chat-header">
@@ -40,4 +81,10 @@ const Chat = () => {
   );
 };
 
-export default Chat;
+const mapStateToProps = ({ auth }) => {
+  return {
+    user: auth.user,
+  };
+};
+
+export default connect(mapStateToProps)(Chat);
